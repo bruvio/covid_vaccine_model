222d7a1ad5d3bf5a1dffbefed14508ede85db1ce
Sourcery AI <>
2022-11-15 14:47:55 +0000

    'Refactored by Sourcery'

--- src/.ipynb_checkpoints/optimizer-checkpoint.py
+++ src/.ipynb_checkpoints/optimizer-checkpoint.py
@@ -5,49 +5,47 @@ from src.epi_model import SIR2, SIR
 def compute_error(y_true,y_pred,which_error='mse',verbose=0):
     '''This function computes the error between expected and predicted Susceptible,Infected,Removed'''
     
-    susc = y_pred[0][0:len(y_true[0])]
-    inf  = y_pred[1][0:len(y_true[0])]
-    rim  = y_pred[2][0:len(y_true[0])]
+    susc = y_pred[0][:len(y_true[0])]
+    inf = y_pred[1][:len(y_true[0])]
+    rim = y_pred[2][:len(y_true[0])]
 
-    
+
     if which_error =='mse':
         err1 = np.sqrt(np.mean((y_true[0]-susc)**2))
         err2 = np.sqrt(np.mean((y_true[1]-inf)**2))
         err3 = np.sqrt(np.mean((y_true[2]-rim)**2))
-        
+
     elif which_error =='mae':
         err1 = np.mean(np.abs(y_true[0]-susc))
         err2 = np.mean(np.abs(y_true[1]-inf))
         err3 = np.mean(np.abs(y_true[2]-rim))
-        
+
     elif which_error =='chi2':
         err1 = np.sqrt(np.mean((y_true[0]-susc)**2/y_true[0]))
         err2 = np.sqrt(np.mean((y_true[1]-inf)**2/y_true[1]))
         err3 = np.sqrt(np.mean((y_true[2]-rim)**2/y_true[2]))
-        
+
     elif which_error == 'perc':
         err1 = np.nanmean(np.abs(y_true[0]-susc)/y_true[0])*100
         err2 = np.nanmean(np.abs(y_true[1]-inf)/y_true[1])*100
         err3 = np.nanmean(np.abs(y_true[2]-rim)/y_true[2])*100
-        
+
         if verbose==1:
             print('Error on susceptible',round(err1,1),'%')
             print('Error on infected',round(err2,1),'%')
             print('Error on removed',round(err3,1),'%')
-        
+
     elif which_error =='weighted':
         err1 = np.sqrt(np.mean((y_true[0]-susc)**2))/2
         err2 = np.sqrt(np.mean((y_true[1]-inf)**2))
         err3 = np.sqrt(np.mean((y_true[2]-rim)**2))/2
-        
+
     elif which_error == 'infected_only':
         err1 = 0
         err2 = np.sqrt(np.mean((y_true[1]-inf)**2))
         err3 = 0
-    
-    errm = (err1+err2+err3)/3
-    
-    return errm
+
+    return (err1+err2+err3)/3
 
 
 def fit_model(y_data,inhabitants,
